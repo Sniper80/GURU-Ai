2c68b85b4b4ae6397ff6aee34f2eb20ff0ff94dd
Sniper80 <75849429+Sniper80@users.noreply.github.com>
2024-07-14 17:10:04 +0300

    Add files via upload

---
+++ plugins/main-Guru.js
@@ -0,0 +1,24 @@
+let handler = async m =>
+  m.reply(
+    `
+
+≡  * 𝚂𝙽𝙸𝙿𝙴𝚁  ┃ ᴮᴼᵀ*   INFO
+
+─────────────
+▢ Join public bot group and support
+https://chat.whatsapp.com/KkMsf03DcKjGRasACd3Se6
+─────────────
+▢ *Owner Telegram*
+ https://t.me/EG_SNIPER
+
+▢ *YouTube*
+• https://www.youtube.com/@EG-SNIPER
+
+
+`.trim()
+  )
+handler.help = ['gpguru']
+handler.tags = ['main']
+handler.command = ['groups', 'groupguru', 'gugp', 'ggp', 'gpguru']
+
+export default handler
